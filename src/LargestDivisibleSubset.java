021c6be18fd51b24c66f80d4610fa7632c58dee4
Ayush Jain <ayushjain@Ayushs-MacBook-Pro.local>
2024-03-31 23:14:47 +0530

    31 march 2024 - 3

---
+++ src/LargestDivisibleSubset.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class LargestDivisibleSubset {
+
+    public static void main(String[] args) {
+        int[] nums = {10, 15, 20};
+        System.out.println(new LargestDivisibleSubset().largestDivisibleSubset(nums));
+    }
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        return null;
+    }
+
+}
